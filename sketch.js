b3b902a6cef7d3833d77d1a99dcad154ec32f140
KARNAMROOPESH <63301556+KARNAMROOPESH@users.noreply.github.com>
2020-04-13 18:20:29 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ function setup() {
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
 
-	helicopterSprite=createSprite(2, 200, 10,10);
+	helicopterSprite=createSprite(width/2, 200, 10,10);
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6
 
